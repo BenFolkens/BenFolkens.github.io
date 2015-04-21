b2df00cd0a8ff178f87bf25ce19ef5adc6f1f848
Ben Folkens <benfolkens@gmail.com>
2015-04-20 22:41:01 -0400

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,17 +1,33 @@
-var HelloWorld = React.createClass({
+var Avatar = React.createClass({
   render: function() {
     return (
-      <p>
-        Hello, <input type="text" placeholder="Your name here" />!
-        It is {this.props.date.toTimeString()}
-      </p>
+      <div>
+        <ProfilePic username={this.props.username} />
+        <ProfileLink username={this.props.username} />
+      </div>
     );
   }
 });
 
-setInterval(function() {
-  React.render(
-    <HelloWorld date={new Date()} />,
-    document.getElementById('example')
-  );
-}, 500);
+var ProfilePic = React.createClass({
+  render: function() {
+    return (
+      <img src={'https://graph.facebook.com/' + this.props.username + '/picture'} />
+    );
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <a href={'https://www.facebook.com/' + this.props.username}>
+        {this.props.username}
+      </a>
+    );
+  }
+});
+
+React.render(
+  <Avatar username="pwh" />,
+  document.getElementById('example')
+);
