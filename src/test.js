4d43f99fe8a115fd9c2c149b95ab7455312cac3e
Ben Folkens <benfolkens@gmail.com>
2015-04-21 18:42:03 -0400

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,36 +1,29 @@
-var TodoList = React.createClass({
-  render: function() {
-    var createItem = function(itemText, index) {
-      return <li key={index + itemText}>{itemText}</li>;
-    };
-    return <ul>{this.props.items.map(createItem)}</ul>;
-  }
-});
-var TodoApp = React.createClass({
+var MarkdownEditor = React.createClass({
   getInitialState: function() {
-    return {items: [], text: ''};
+    return {value: 'Type some *markdown* here!'};
   },
-  onChange: function(e) {
-    this.setState({text: e.target.value});
-  },
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var nextItems = this.state.items.concat([this.state.text]);
-    var nextText = '';
-    this.setState({items: nextItems, text: nextText});
+  handleChange: function() {
+    this.setState({value: React.findDOMNode(this.refs.textarea).value});
   },
   render: function() {
     return (
-      <div>
-        <h3>TODO</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.onChange} value={this.state.text} />
-          <button>{'Add #' + (this.state.items.length + 1)}</button>
-        </form>
+      <div className="MarkdownEditor">
+        <h3>Input</h3>
+        <textarea
+          onChange={this.handleChange}
+          ref="textarea"
+          defaultValue={this.state.value} />
+        <h3>Output</h3>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={{
+            __html: marked(this.state.value, {sanitize: true})
+          }}
+        />
       </div>
     );
   }
 });
 
-React.render(<TodoApp />, mountNode);
+React.render(<MarkdownEditor />, mountNode);
+
